88e1e81b36f698e73495f04ba7ead87175c768be
Jaakko Ojalehto <jaakko@ojalehto.fi>
2014-02-08 12:03:41 +0200

    Encoded images with base64 and moved them to the css and html files. Changed links to open into new windows with size respective to the social network. Added google+.

--- js/share-buttons.js
+++ js/share-buttons.js
@@ -18,12 +18,29 @@ var shareButton = (function() {
 
     function open(e, args) {
         e.preventDefault();
-        window.open(args.url + encodeURIComponent(window.location), '_blank');
+        window.open(args.url + encodeURIComponent(window.location), '_blank', 'toolbar=0,location=1,directories=1,status=1,menubar=0,scrollbars=1,resizable=1,width=' + args.width + ',height=' + args.height);
     }
 
-    bind(document.getElementById('share-button-facebook'), 'click', open, {url: 'https://www.facebook.com/sharer/sharer.php?u='});
-    bind(document.getElementById('share-button-twitter'), 'click', open, {url: 'https://twitter.com/intent/tweet?text='});
-    bind(document.getElementById('share-button-linkedin'), 'click', open, {url: 'http://www.linkedin.com/shareArticle?url='});
+    bind(document.getElementById('share-buttons-facebook'), 'click', open, {
+        url: 'https://www.facebook.com/sharer/sharer.php?u=',
+        width: 650,
+        height: 313
+    });
+    bind(document.getElementById('share-buttons-twitter'), 'click', open, {
+        url: 'https://twitter.com/intent/tweet?text=',
+        width: 680,
+        height: 260
+    });
+    bind(document.getElementById('share-buttons-linkedin'), 'click', open, {
+        url: 'http://www.linkedin.com/shareArticle?url=',
+        width: 600,
+        height: 500
+    });
+    bind(document.getElementById('share-buttons-googleplus'), 'click', open, {
+        url: 'https://plus.google.com/share?url=',
+        width: 484,
+        height: 510
+    });
 
     var script = document.createElement('script');
     script.src = 'http://ojalehto.fi/share-counter/shares/total?url=' + encodeURIComponent(window.location) + '&id=1&callback=shareButton.saveTotal';
@@ -31,7 +48,10 @@ var shareButton = (function() {
 
     return {
         saveTotal: function(jsonp) {
-            document.getElementById('share-count').innerHTML = jsonp.all;
+            if (typeof(jsonp.all) !== 'undefined') {
+                document.getElementById('share-buttons-share-count').innerHTML = jsonp.all;
+                document.getElementById('share-buttons-share-count').style.display = 'block';
+            }
         }
     };
 
